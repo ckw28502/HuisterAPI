22270f318d085415de16b61ce9f8270850618ccd
ckw28502 <ckw28502@gmail.com>
2023-09-20 10:57:03 +0200

    feat : change requirement city will be created when city not found in repository during property creation

--- src/main/java/nl/fontys/s3/huister/business/impl/property/CreatePropertyUseCaseImpl.java
+++ src/main/java/nl/fontys/s3/huister/business/impl/property/CreatePropertyUseCaseImpl.java
@@ -3,6 +3,7 @@ package nl.fontys.s3.huister.business.impl.property;
 import lombok.AllArgsConstructor;
 import nl.fontys.s3.huister.business.interfaces.property.CreatePropertyUseCase;
 import nl.fontys.s3.huister.domain.request.property.CreatePropertyRequest;
+import nl.fontys.s3.huister.persistence.CityRepository;
 import nl.fontys.s3.huister.persistence.PropertyRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,21 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CreatePropertyUseCaseImpl implements CreatePropertyUseCase {
     private final PropertyRepository propertyRepository;
+    private final CityRepository cityRepository;
 
     @Override
     public void createProperty(CreatePropertyRequest request) {
+        int cityId;
+        String cityName=request.getCityName();
+
+        //Check if city already in repository
+        if (cityRepository.cityNameExists(cityName)){
+            cityId=cityRepository.getCityByName(cityName).get().getId();
+        }else {
+            cityId=cityRepository.createCity(cityName);
+        }
+
+        request.setCityId(cityId);
         propertyRepository.createProperty(request);
     }
 }
